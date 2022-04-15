86388bfcd7634137ffc744b2a30cfee65f72b812
ConfiG <cgytrus@cgyt.ru>
2022-04-15 18:27:00 +0300

    add scripts

---
+++ scripts/bat/mods.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call .\add_mod.bat WysApi
+call .\add_mod.bat WysModMenu
+call .\add_mod.bat WysExampleMod
---
+++ scripts/bat/mods_no_example.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call .\add_mod.bat WysApi
+call .\add_mod.bat WysModMenu
+call .\remove_mod.bat WysExampleMod
